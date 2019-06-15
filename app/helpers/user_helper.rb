a1ef67a946131b5f9d1296692e356301bb4b94a9
dallas <dallakyanas@gmail.com>
2019-06-15 12:10:11 +0300

    Добавить пользователям настройку подписки
    Fixes #3
    
    Завел в модели новое целочисленное поле, сделал его перечислимым (типа
    чтоб не париться) - no, daily, weekly. after_initialize заполняется в weekly.
    
    Сделал хелпер, чтоб в дропбоксах отображать опции по-человечески
    (попутно костыльно локализовал)
    
    Email в хедере сделал ссылкой на страницу редактирования пользователя.
    На форму редактирования втсавил селект для нового поля (с хелпером)
    
    Чтобы разрешить редактирвать новое поле, пришлось пронаследовать
    контроллер от Divise и вставить в нег пермит на новое поле. Изменения в
    routes.rb тоже с этим связаны

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,12 @@
+module UserHelper
+  # TODO: костыль, но пока так
+  def map_subscription_name(key)
+    name_map = {
+                'no'     =>'Отключена',
+                'daily'  =>'Ежедневная',
+                'weekly' =>'Еженедельная',
+               }
+
+    return name_map[key]
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum subscription_type: [:no, :daily, :weekly]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   after_create :send_welcome_email
+  after_initialize :init
+
+
+  def init
+    self.subscription_type ||= :weekly
+  end
+
 
   def send_welcome_email
     UserMailer.with(user: self).welcome_email.deliver_later
+  end
+
+
+  def is_admin?
+    # кто первый встал того и сервер =)
+    self.id == 1
   end
 end
