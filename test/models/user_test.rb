939added0bab4eecf837d3120020dfed9721cba7
dallas <dallakyanas@gmail.com>
2019-06-17 00:13:00 +0300

    Написать один любой тест
    Fixes #4
    
    Добавил элементарные тесты на обязательность email и редирект при входе
    неавторизхованным

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,25 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "should be valid (fixture)" do
+    user = users(:daily_user)
+    assert user.valid?
+  end
+
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
 end
