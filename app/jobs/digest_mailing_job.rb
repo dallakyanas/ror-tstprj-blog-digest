0e3de6994f225b7bf8445d113a39721e31be7e79
dallas <dallakyanas@gmail.com>
2019-07-03 03:15:35 +0300

    Разбить digest_mailing_job.rb на иерархию классов
    Fixes #26
    
    Сделал предка джоба DigestMailingJob в котором отбираю коллекции для
    рассылки, потомки задают параметры и вызывает метод проведения рассылки,
    используя полиморфизм
    
    ДОбавил а форму регистрации пользователя "Имя" + добавил поле в
    резрешенные

--- app/jobs/digest_mailing_job.rb
+++ app/jobs/digest_mailing_job.rb
@@ -1,44 +1,15 @@
-class DailyDigestMailingJob < ApplicationJob
+class DigestMailingJob < ApplicationJob
   queue_as :default
 
-  def perform
-    users_daily_digest = User.where(subscription_type: 'daily')
-
-    if !users.empty?
-      # отберем посты для расылки
-      till_date = Time.parse("07:00") - 1  # сегодня
-      from_date = till_date - 1.day + 1
-
-      post_daily_digest = BlogPost.where(publish_date: from_date..till_date).order(publish_date: :desc)
-
-      if !post_daily_digest.empty?
-        users_daily_digest.each do |u|
-          UserMailer.with(user: u, blog_posts: post_daily_digest).daily_digest_email.deliver_later
-        end
-      end
-    end  #/!users.empty?
-
-  end #/perform
-end
-
-
-class WeeklyDigestMailingJob < ApplicationJob
-  queue_as :default
+  attr_accessor :subscr_type, :from_date, :till_date
 
   def perform
-    users_weekly_digest = User.where(subscription_type: 'weekly')
-    if !users.empty?
-      # отберем посты для расылки
-      till_date = Time.parse("07:00") - 1  # сегодня
-      from_date = till_date - 1.week + 1
+    @recipients = User.where(subscription_type: @subscr_type)
 
-      post_weekly_digest = BlogPost.where(publish_date: from_date..till_date).order(publish_date: :desc)
+    @posts = BlogPost.where(publish_date: @from_date..@till_date).order(publish_date: :desc)
 
-      if !post_weekly_digest.empty?
-        users_weekly_digest.each do |u|
-          UserMailer.with(user: u, blog_posts: post_weekly_digest).weekly_digest_email.deliver_later
-        end
-      end
-    end  #/!users.empty?
+    @recipients.each do |r|
+      do_mailing(r, @posts) unless @posts.empty? # abstract do_mailing
+    end
   end #/perform
 end
